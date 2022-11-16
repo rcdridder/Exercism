80812998d2fec720b239988f5dc40c2bfe52d792
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-11-16 19:12:44 +0100

    Finished Resistor Color.

---
+++ Exercism/Arrays/ResistorColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Arrays
+{
+    public static class ResistorColor
+    {
+        private static string[] resistorColors = {"black" , "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        public static int ColorCode(string color) => Array.IndexOf(resistorColors, color);
+        public static string[] Colors() => resistorColors;
+    }
+}
