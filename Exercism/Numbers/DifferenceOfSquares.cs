ba41e6ee06de7ccf473a3efad8c7b3c275b8af3f
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-11-17 11:44:22 +0100

    Difference Of Squares solved.

---
+++ Exercism/Numbers/DifferenceOfSquares.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Numbers
+{
+    public static class DifferenceOfSquares
+    {
+        public static int CalculateSquareOfSum(int max) => (max * (max + 1) / 2) * (max * (max + 1) / 2);
+
+        public static int CalculateSumOfSquares(int max) => (max * (max + 1) * (2 * max + 1)) / 6;
+
+        public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
