2dd92a30f6fed93e06d5336a37766bdca89c9ecb
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-12-02 13:41:58 +0100

    Roman Numerals solved.

---
+++ Exercism/Numbers/RomanNumeralsNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercism.Numbers
+{
+    public static class RomanNumeralsNumbers
+    {
+        public static string ToRomanNumbers(this int value)
+        {
+            string[] roman1s = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] roman10s = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] roman100s = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] roman1000s = { "", "M", "MM", "MMM" };
+
+            int thousands = Convert.ToInt32(Math.Floor(value / 1000d));
+            int hundreds = Convert.ToInt32(Math.Floor((value - thousands * 1000) / 100d));
+            int tens = Convert.ToInt32(Math.Floor((value - thousands * 1000 - hundreds * 100) / 10d));
+            int singles = value % 10;
+
+            return $"{roman1000s[thousands]}{roman100s[hundreds]}{roman10s[tens]}{roman1s[singles]}";
+        }
+    }
+}
--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Exercism.Arrays;
 using Exercism.ExtensionMethods;
-using Exercism.Numbers;
-using Exercism.Strings;
 
-var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
-var value = 1;
-
-Console.WriteLine(BinarySearchArray.Find(array, value));
+int value = 2999;
+Console.WriteLine(value.ToRomanExtension());
