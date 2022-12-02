ecf74525ce8527ff23cb292f4d3e44f8ade76920
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-12-02 23:57:36 +0100

    Phone Number Analysis solved.

---
+++ Exercism/Tuples/PhoneNumberAnalysis.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercism.Tuples
+{
+    public static class PhoneNumberAnalysis
+    {
+        public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+        {
+            string[] sections = phoneNumber.Split('-');
+            return (sections[0] == "212", sections[1] == "555", sections[2]);
+        }
+
+        public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake ? true : false;
+
+    }
+}
