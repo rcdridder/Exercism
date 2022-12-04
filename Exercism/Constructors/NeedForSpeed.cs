f43b6caadde8119e88441598af6c70c391533f22
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-12-04 01:13:37 +0100

    Need For Speed solved.

---
+++ Exercism/Constructors/NeedForSpeed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Constructors
+{
+    internal class NeedForSpeed
+    {
+    }
+}
