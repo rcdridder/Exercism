5e9d8cc14de36f2146536069648aa13dccd43a93
Rob de Ridder <rderidder@outlook.com>
2023-02-01 13:17:05 +0100

    Protein Translation solved.

--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Exercism.FloatingPointNumbers;
 using Exercism.ForeachLoops;
+using Exercism.WhileLoops;
 
-IEnumerable<int> books = new List<int>() { 5, 4, 3, 3, 2, 2, 1, 1, 5, 4, 3, 3, 2, 2, 1, 1 };
-Console.WriteLine(BookStoreFloat.Total(books)); 
+Console.WriteLine(ProteinTranslation.Proteins("UGGUAG")); 
