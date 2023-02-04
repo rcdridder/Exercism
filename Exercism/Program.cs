29ef16175bdfa96dd18c0b099ed2cad8db3703a5
Rob de Ridder <rderidder@outlook.com>
2023-02-04 16:59:46 +0100

    Squeaky Clean solved.

--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -1,8 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Exercism.FloatingPointNumbers;
-using Exercism.ForeachLoops;
-using Exercism.Randomness;
-using Exercism.WhileLoops;
+using Exercism.Chars;
 
-RobotNameRandom robot = new RobotNameRandom();
-Console.WriteLine(robot.Name);
+Console.WriteLine(SqueakyCleanChars.Clean("my\0Id"));
