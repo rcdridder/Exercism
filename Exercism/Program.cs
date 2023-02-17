c4b1d065c37cb173de931f4eee160544a2bc6cec
Rob de Ridder <rderidder@outlook.com>
2023-02-17 11:25:15 +0100

    Atbash Cipher solved.

--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Exercism.Chars;
 using Exercism.Constructors;
+using Exercism.ForeachLoops;
 using Exercism.Strings;
 
-RailFenceCipher cipher = new(3);
-Console.WriteLine(cipher.Encode("WEAREDISCOVEREDFLEEATONCE"));
-Console.WriteLine(cipher.Decode("WECRLTEERDSOEEFEAOCAIVDEN"));
+Console.WriteLine(AtbashCipher.Encode("Ariana Grande"));
+Console.WriteLine(AtbashCipher.Decode("zirzmztizmwv"));
