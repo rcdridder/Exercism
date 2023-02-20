6eef95a908be25a3164708e8fe51c4cd311373ca
Rob de Ridder <rderidder@outlook.com>
2023-02-20 02:33:03 +0100

    Isogram solved.

--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Exercism.Chars;
-using Exercism.Constructors;
 using Exercism.ForeachLoops;
-using Exercism.Strings;
 
 Console.WriteLine(AtbashCipher.Encode("Ariana Grande"));
 Console.WriteLine(AtbashCipher.Decode("zirzmztizmwv"));
